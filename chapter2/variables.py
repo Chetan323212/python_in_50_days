43208ee4fa0b593d958c492b838cd063a580d11f
Chetan323212 <chetanpatil323212@gmail.com>
2025-08-13 20:02:33 +0530

    Create variables.py

---
+++ chapter2/variables.py
@@ -0,0 +1,12 @@
+name = "sagar"
+age = 22  # age , name . hight -> is a variables
+hight = 5.2 # it is a float vairable 
+
+print(name, age , hight)  
+
+# -> is used for an comment single line comment 
+
+Boolean = False  # boolean is a data type in python
+
+print(Boolean) # it will print false
+
