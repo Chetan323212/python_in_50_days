860d495bf6986520d88ab829d3e825c878c24d91
Chetan323212 <chetanpatil323212@gmail.com>
2025-08-15 19:49:29 +0530

    Update variables.py

--- chapter2/variables.py
+++ chapter2/variables.py
@@ -1,12 +1,28 @@
-name = "sagar"
-age = 22  # age , name . hight -> is a variables
-hight = 5.2 # it is a float vairable 
+# name = "sagar"
+# age = 22  # age , name . hight -> is a variables
+# hight = 5.2 # it is a float vairable 
 
-print(name, age , hight)  
+# print(name, age , hight)  
 
 # -> is used for an comment single line comment 
 
-Boolean = False  # boolean is a data type in python
+# Boolean = False  # boolean is a data type in python
+# 
+# print(Boolean) # it will print false
 
-print(Boolean) # it will print false
+my_list = ["chetan", "patil", 30_09_2004, "om"]
+print(type(my_list))
+
+my_tuple = ("chetan","patil",30_09_2004, "om")
+print(type(my_tuple))
+
+my_set = {1,2,2,3,4,5,6,7,8,9,0,2,4,5,6,7,8,9,1,}
+print(type(my_set))
+
+my_mapp = {
+    
+    'name':'chetan', 'age':'30', 'hight':'5.2'
+    
+    }
+print(type(my_mapp))  # it will print {name:chetan, age:30,}
 
