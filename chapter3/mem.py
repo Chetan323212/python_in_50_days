8dcf9d9a70324da09794ad92907b99254610090a
Chetan323212 <chetanpatil323212@gmail.com>
2025-08-27 16:01:02 +0530

    operators

---
+++ chapter3/mem.py
@@ -0,0 +1,17 @@
+"""
+this type of operator is used for cheack momory location is same or not.
+ 
+is 
+is not 
+"""
+
+a = [1,2,3,4,5]
+b = a
+
+c = [1,2,3,4,5]
+
+print(a is b) # the output well be true 
+print(a is c )# the output well be false
+
+
+
---
+++ chapter3/membership.py
@@ -0,0 +1,11 @@
+"""
+    Membership operator is used for cheack the element in the list or array
+    
+    in 
+    not in
+"""
+
+Roll_number = [10,11,12,13,14,15,16,17]
+
+print(10 in Roll_number)
+print(20 in Roll_number)
